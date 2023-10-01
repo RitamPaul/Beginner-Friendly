ae1de9988dbf98fb2e809f40b76b3529638e9de6
Jishnudeep Borah <85721902+Jishnu2608@users.noreply.github.com>
2023-10-02 01:32:42 +0530

    Create MergeStringsAlternately.java

---
+++ CODE/Data Structures & Algorithms/MergeStringsAlternately.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/merge-strings-alternately/description/
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+
+        int maxLength = Math.max(word1.length(), word2.length());
+
+        for(int i=0;i<maxLength;i++){
+            if(i<word1.length()){
+                sb.append(word1.charAt(i));
+            }
+
+            if(i<word2.length()){
+                sb.append(word2.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
