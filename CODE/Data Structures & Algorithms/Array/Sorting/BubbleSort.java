e937207ba4786e45b4be0673898b6de73ed3843f
Shubham Goyal <shubhamgoyal7125@gmail.com>
2023-10-04 22:52:05 +0530

    Added Bubble Sort program

---
+++ CODE/Data Structures & Algorithms/Array/Sorting/BubbleSort.java
@@ -0,0 +1,30 @@
+public class BubbleSort { 
+	void bubbleSort(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i = 0; i < n - 1; i++) 
+			for (int j = 0; j < n - i - 1; j++) 
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j]; 
+					arr[j] = arr[j + 1]; 
+					arr[j + 1] = temp; 
+				} 
+	} 
+
+	void printArray(int arr[]) 
+	{ 
+		int n = arr.length; 
+		for (int i = 0; i < n; ++i) 
+			System.out.print(arr[i] + " "); 
+		System.out.println(); 
+	} 
+
+	public static void main(String args[]) 
+	{ 
+		BubbleSort ob = new BubbleSort(); 
+		int arr[] = { 6, 54, 4, 1, 42, 21}; 
+		ob.bubbleSort(arr); 
+		System.out.println("Sorted array"); 
+		ob.printArray(arr); 
+	} 
+}
