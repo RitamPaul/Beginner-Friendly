de9d905c71d16ac32f69841452268dee4f60c468
RitamPaul <ritampaulblogpost@gmail.com>
2023-10-12 11:21:54 +0530

    added question statement

--- CODE/Data Structures & Algorithms/Basics/max_xor.java
+++ CODE/Data Structures & Algorithms/Basics/max_xor.java
@@ -1,3 +1,18 @@
+/*
+
+Question :- Maximum XOR subarray
+
+Find the subarray from a given array with maximum XOR. A subarray is a contiguous part of the array.
+
+Example:-
+
+arr[] = {1,2,3,4}
+Output: 7
+Explanation: 
+The subarray {3,4} has maximum xor value equal to 7.
+
+*/
+
 class xor {
 	static int max_xor(int arr[], int n){
 		int maxXor = 0;
