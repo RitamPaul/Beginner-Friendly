7a07984137faf893c1f87d77f8ca1f3e35973faa
RitamPaul <ritampaulblogpost@gmail.com>
2023-10-16 15:58:17 +0530

    added question

--- CODE/Data Structures & Algorithms/Basics/pascalTriangleII.java
+++ CODE/Data Structures & Algorithms/Basics/pascalTriangleII.java
@@ -1,3 +1,34 @@
+/*
+QUESTION:- 119. Pascal's Triangle II    [From Leetcode]
+
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+                                1
+                               / \
+                              1   1
+                             / \ / \
+                            1   2   1
+                           / \ / \ / \
+                          1   3   3   1
+                         / \ / \ / \ / \
+                        1   4   6   4   1
+
+Example-1:-
+Input: rowIndex = 3
+Output: [1,3,3,1]
+
+Example-2:-
+Input: rowIndex = 0
+Output: [1]
+
+Example-3:-
+Input: rowIndex = 1
+Output: [1,1] 
+
+Constraints:- 0 <= rowIndex <= 33
+*/
+
 class Solution {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> res = new ArrayList<>();
