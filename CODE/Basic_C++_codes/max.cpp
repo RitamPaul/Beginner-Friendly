2f0fb9c772dcccd9db84adc2475258f6928d4ae4
Sivia <anzalsivia56@gmail.com>
2023-10-21 00:52:29 +0530

    Maximum number in an array using C++

---
+++ CODE/Basic_C++_codes/max.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std; 
+int maximum(int a[], int n)
+{
+    if(n==0)
+    {
+        return a[n];
+        return max(a[n-1], maximum(a, n-1));
+    }
+    int max = maximum(a, n-1);
+    if(max<a[n])
+        max = a[n];
+        return max;
+}
+int main()
+{
+    int a[] = {1, 4, 35, 6, -20, 5, 2};
+    int n = 5;
+    cout<<maximum(a, n)<<endl;
+    return 0;
+}
