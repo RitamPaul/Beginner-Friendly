10db8be68b7dbeef22b646b86ab00c8c1018ecab
Gregor Davies <43633955+Gregor-Davies@users.noreply.github.com>
2023-10-25 11:03:58 +0100

    Delete CODE/Bulk Image Resizer Python/resizer.py

--- CODE/Bulk Image Resizer Python/resizer.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import pathlib
-maxsize = (312, 312)
-for input_img_path in pathlib.Path("input").iterdir():
-    output_img_path = str(input_img_path).replace("input","output")
-    with Image.open(input_img_path) as im:
-        im.thumbnail(maxsize)
-        im.save(output_img_path, "JPEG", dpi=(300,300))
-        print(f"processing file {input_img_path} done...")
